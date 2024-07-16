98a002cbda6299fdf98e0f6ad07230fe15b42070
shubhat33 <rashidshubhat41@gmail.com>
2024-07-16 12:13:24 +0530

    making styled links for navigation

--- nextjs-routing/src/app/products/page.tsx
+++ nextjs-routing/src/app/products/page.tsx
@@ -1,15 +1,31 @@
+"use client"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import './styles.css'
+type product = {
+  serial : number,
+  href : string
+}
+
+const products : product [] = [
+  {serial:1,href:'/products/01'},
+  {serial:2,href:'/products/02'},
+  {serial:3,href:'/products/03'}
+]
 
 const Products = () => {
+  const currentpath = usePathname()
   return (
     <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
         <Link href="/">Home</Link>
         <h1>
             PRODUCTS LIST
         </h1>
-        <Link href="/products/01">P1</Link>
-        <Link href="/products/02">P2</Link>
-        <Link href="/products/03">P3</Link>
+        {
+          products.map((product,index) => (
+            <Link href={product.href} key={index} className="font-bold text-red-500 hover:text-blue-300 ">Product 0{product.serial}</Link>
+          ))
+        }
     </div>
   )
 }
