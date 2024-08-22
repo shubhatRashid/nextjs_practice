501c0d18ec0168ed0b42899ba0888cb1e4731c75
shubhat33 <rashidshubhat41@gmail.com>
2024-08-22 12:01:48 +0530

    patch request

--- route-handlers/src/app/api/[id]/route.ts
+++ route-handlers/src/app/api/[id]/route.ts
@@ -1,14 +1,18 @@
 import data from "../data"
 
-export async function GET(request:Request,{params}:{params:{id:string}}) {
+export async function PATCH(request:Request,{params}:{params:{id:string}}) {
     try {
         const item = data.find((eachitem)=>eachitem.id == parseInt(params.id))
         if (item == null){
             throw new Error('something unexpected happned')
         }
+        const newData = await request.json()
+        item.description = newData.description
         return new Response(JSON.stringify(item))
+
     } catch (error) {
         return new Response(error.message)
     }
    
 }
+
