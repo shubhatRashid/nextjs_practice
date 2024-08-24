6d4feb91d3563a5ae878839213ee2010aa1440d0
shubhat33 <rashidshubhat41@gmail.com>
2024-08-24 16:44:57 +0530

    changing headers in response

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -4,5 +4,11 @@ export function GET(request:NextRequest) {
     const allheaders = headers()
     console.log(allheaders.get('Authorization'))
     console.log(request.headers.get('Authorization'))
-    return Response.json('headers test')
+    return Response.json('<h1>headers test</h1>',{
+        headers:{
+            "Content-Type" : 'text/html',
+            'Authorization':'Bearer 456'
+        },
+        
+    })
 }
