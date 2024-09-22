d1027fa7c34e8c8451cc3ff89ceb549e3a1a9585
shubhat33 <rashidshubhat41@gmail.com>
2024-09-22 13:33:25 +0530

    complete project

--- imagecarousel/src/components/NewCarousel.tsx
+++ imagecarousel/src/components/NewCarousel.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import '../styles.css'
 import { useEffect, useState } from "react";
+import { url } from "inspector";
 type image = {
     src:string,
     alt:string,
@@ -11,17 +12,15 @@ type image = {
     export default function NewCarousel({images}:{images:image[]}) {
         const [allImages,setAllImages] = useState(images)
         const [current,setCurrent] = useState(0)
-        const [change,setChange] = useState(false)
 
         const next = () => {
-            setCurrent((curr) => (curr < allImages.length - 1 ? curr + 1 : curr)); 
-            setChange(true)
+            setCurrent((curr) => (curr < allImages.length - 1 ? curr + 1 : curr));
         }
         
         const prev = () => {
-            setCurrent((curr) => (curr > 0 ? curr - 1 : curr))
-            setChange(true)
+            setCurrent((curr) => (curr > 0 ? curr - 1 : curr)); 
         }
+        
 
          const handleKeyDown = (event: KeyboardEvent) => {
             if (event.key === 'ArrowRight') next();
@@ -30,41 +29,49 @@ type image = {
         useEffect(()=>{
             window.addEventListener('keydown',handleKeyDown)
             return () => window.removeEventListener('keydown',handleKeyDown)
-        },[change])
-
-        useEffect(() => {
-            setChange(false)
-        },[change])
+        },[current,allImages])
 
         return (
-            <div className="relative flex justify-center items-center gap-10 h-screen w-[100%] overflow-hidden">
+            <div 
+                className="relative flex justify-center items-center gap-10 h-screen w-[100%] overflow-hidden"
+            >
                 {
                     allImages.map((eachimage,index) => (
                             <div 
-                                id={`${index}`}
+                                id= {`{${index}}`}
                                 key={index} 
-                                className="absolute rounded-lg w-[200px] h-[300px] z-100"
+                                className="absolute rounded-lg w-[200px] h-[300px]" 
                                 style={{
-                                    '--index':index,
-                                    transform: `translateX(${(index)*220}px)`,
-                                    animation:(index === current) ? 'zoom 1s forwards':'slide 2s forwards',
+                                    transform: `translateX(${(index-current)*220}px)`,
+                                    transition: 'transform 1s ease-in-out',
+                                    animation:(index <= current) ? 'scaleUp 1s forwards':'scaleDown 1s forwards'
                                 }}
                                 >
-                            <Image
-                                fill
-                                objectFit="cover"
-                                src={eachimage.src}
-                                alt={eachimage.alt} 
-                                style={{
-                                    borderRadius:index!=0?'25px':'0',
-                                }}
-                                
-                            />
+                                <Image
+                                    fill
+                                    objectFit="cover"
+                                    src={eachimage.src}
+                                    alt={eachimage.alt} 
+                                    style={{
+                                        borderRadius:index!=0?'25px':'0',
+                                    }}
+                                    
+                                />
+                                {index === current && (
+                                    <div 
+                                        id="description"
+                                        className="absolute flex flex-col  text-gray-100 font-serif top-[15%] right-[60%] left-[10%]"
+                                        style={{animation:'slide 1s ease'}}
+                                    >
+                                        <h1 className="capitalize text-7xl font-bold">{images[current].alt}</h1>
+                                        <p className="pl-5 pt-5">{images[current].description}</p>
+                                    </div>  
+                                )}
                             </div>
                                 
                     ))
                 }
-            <div className="absolute flex w-full h-full justify-center items-end gap-10">
+            <div className="absolute flex justify-center items-center w-full h-full gap-10 right-[30%] top-[10%] opacity-[40%]">
                 <button onClick={prev} className="bg-white p-3 rounded-full font-bold text-xl" >
                     <h1>{"<"}</h1>
                 </button>
